01dfdebaf67b6e26b9247aeec691e58c9d96cdb9
Tom Oliveira <washington-kd@hotmail.com>
2020-10-11 01:17:52 -0300

    add env port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const cors = require('cors');
 const express = require('express');
 const routes = require('./router');
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(express.json());
@@ -9,4 +11,4 @@ app.use(cors());
 app.use(express.urlencoded());
 app.use(routes);
 
-app.listen(5000);
+app.listen(PORT);
